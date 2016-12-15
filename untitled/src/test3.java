96bd2997f8b807a8f5745519e6b55f0093a6b25c
Kasia <kasia@wiseman.pl>
2016-12-15 17:57:52 +0100

    testuje wysyłanie na git

--- untitled/src/test3.java
+++ untitled/src/test3.java
@@ -3,13 +3,13 @@
  */
 public class test3 {
     public static void main(String[] args) {
-        System.out.print("Liczba int: ");
+        System.out.println("Liczba int: ");
         System.out.println(25);
 
-        System.out.print("Liczba double: ");
+        System.out.println("Liczba double: ");
         System.out.println(25.75);
 
-        System.out.print("Znak A: ");
+        System.out.println("Znak A: ");
         System.out.println('A');
 
         System.out.print("Czy uczę się programowania? ");
